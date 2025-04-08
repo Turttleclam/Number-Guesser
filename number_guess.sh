b7ee8bb7363062e3543750b9cfa0cac8a74ad2b9
Turttleclam <tomlinsonkram@gmail.com>
2025-04-08 19:46:07 +0000

    feat: adding user handling

--- number_guess.sh
+++ number_guess.sh
@@ -5,7 +5,10 @@
 # Prompt for a username
 echo -e "\nEnter your username:"
 read USER_NAME
-
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE user_name = '$USER_NAME'")
+if [[ -z $USER_ID ]]
+then
+  echo -e "\nWelcome, $USER_NAME! It looks like this is your first time here."
 
 
 
