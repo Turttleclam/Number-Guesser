de59df5d0c29fcc3eda36f068316a0081fc260df
Turttleclam <tomlinsonkram@gmail.com>
2025-04-08 20:20:25 +0000

    feat: add handle for returning user

--- number_guess.sh
+++ number_guess.sh
@@ -12,6 +12,11 @@ if [[ -z $USER_ID ]]
 then
   INSERT_NEW_USER=$($PSQL "INSERT INTO users(user_name) VALUES('$USER_NAME')")
   echo -e "\nWelcome, $USER_NAME! It looks like this is your first time here."
+else
+  GAMES_PLAYED=$($PSQL "SELECT COUNT(game_id) FROM games WHERE user_id = $USER_ID")
+  BEST_GAME=$($PSQL "SELECT MIN(score) FROM games WHERE user_id = $USER_NAME")
+  echo -e "\nWelcome back, $USER_NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
 
 
 
