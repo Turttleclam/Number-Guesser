8c09191aac3d218a229841fd8662ac7b1dab2bcd
Turttleclam <tomlinsonkram@gmail.com>
2025-04-08 19:54:50 +0000

    feat: added insert new user info

--- number_guess.sh
+++ number_guess.sh
@@ -5,10 +5,14 @@
 # Prompt for a username
 echo -e "\nEnter your username:"
 read USER_NAME
+# Check for user_id
 USER_ID=$($PSQL "SELECT user_id FROM users WHERE user_name = '$USER_NAME'")
+# If this is a new user
 if [[ -z $USER_ID ]]
 then
+  INSERT_NEW_USER=$($PSQL "INSERT INTO users(user_name) VALUES('$USER_NAME')")
   echo -e "\nWelcome, $USER_NAME! It looks like this is your first time here."
+
 
 
 
