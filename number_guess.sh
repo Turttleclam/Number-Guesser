020e173bfa62f2207f380bf7c76f9c29fb3ca8dd
Turttleclam <tomlinsonkram@gmail.com>
2025-04-10 02:01:39 +0000

    fix: mistake at line 55

--- number_guess.sh
+++ number_guess.sh
@@ -52,7 +52,6 @@ else
     INSERT_SCORE=$($PSQL "INSERT INTO games(score) VALUES($SCORE)")
     echo -e "\nYou guessed it in $SCORE tries. The secret number was $RANDOM_NUMBER. Nice job!"
     fi
-  done
 fi
 
 
